084bc345d94f98b2d9f1e4431a46a63d4d696a63
ZofiaDrabek <zosia.drabek@gmail.com>
2023-04-06 15:11:58 +0200

    Initial Commit

---
+++ TidalChallenge/TidalChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TidalChallengeApp.swift
+//  TidalChallenge
+//
+//  Created by Zofia Drabek on 06.04.23.
+//
+
+import SwiftUI
+
+@main
+struct TidalChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
